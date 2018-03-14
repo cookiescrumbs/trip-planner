b45aa924813caac6025be35f9ef1f9e5318b977c
Steven Cook <stevenjamescook@gmail.com>
2018-03-14 22:01:27 +0000

    feature changes

--- e2e/steps/plan-a-trip.step.ts
+++ e2e/steps/plan-a-trip.step.ts
@@ -3,5 +3,4 @@ import { defineSupportCode } from 'cucumber';
 defineSupportCode(({ Given, When, Then, Before }) => {
   Given('an angler has only one day to plan a fishing trip', () => {
   });
-
 });
